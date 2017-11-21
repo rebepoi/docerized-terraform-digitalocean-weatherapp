83e43776b8302d3f915716d39e331a229a5d60f1
Reino <reino@prospercart.fi>
2017-11-21 19:15:00 +0200

    Swarm deploy setup

--- weatherapp.tf
+++ weatherapp.tf
@@ -35,15 +35,21 @@ resource "digitalocean_droplet" "weatherapp" {
    "export PATH=$PATH:/usr/bin",
    "service docker start",
    "cd ~/",
-   "git clone https://github.com/rebepoi/forecast",
+   "git clone -b with-swarm --single-branch https://github.com/rebepoi/forecast",
    "curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
    "chmod +x /usr/local/bin/docker-compose",
    "cd forecast",
+   "docker swarm init --advertise-addr ${digitalocean_droplet.weatherapp.ipv4_address}",
+   "docker service create --name registry -p 5000:5000 registry:2",
    "sed -i -e 's/0.0.0.0:9000/${digitalocean_droplet.weatherapp.ipv4_address}:9000/g' ./frontend/webpack.config.js",
-   "docker-compose up -d"
+   "docker-compose up -d",
+   "docker-compose down --volumes",
+   "docker-compose push",
+   "docker stack deploy -c docker-compose.yml weatherapp",
+   "docker run -it -d -p 81:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer"
   ]
  }
 }
 output "ip_v4_address_public" {
-    value = "${digitalocean_droplet.weatherapp.ipv4_address}"
+    value = "check forecast from: ${digitalocean_droplet.weatherapp.ipv4_address} and swarm from: ${digitalocean_droplet.weatherapp.ipv4_address}:81"
 }
